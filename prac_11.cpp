0309cecdc4a66675734ec2e2d2261f6700346219
Tanushka Patil <94859584+tanushkapatil@users.noreply.github.com>
2024-11-20 15:20:05 +0530

    Update prac_11.cpp

--- prac_11.cpp
+++ prac_11.cpp
@@ -56,3 +56,55 @@ public :
     void displayTable();
     int hashFunction(int emp_id);
 };
+int HashTable::hashFunction(int emp_id) {
+    return emp_id%size ;
+}
+
+void HashTable::insertID() {
+    Employee newEmp ;
+    newEmp.read() ;
+    int emp_id = newEmp.getID() ;
+    int pos = hashFunction(emp_id) ;
+    for(int i = 0 ; i < size ; i++) {
+        pos = (pos + i) % size ;
+        if(flag[pos] == 0){
+            e[pos] = newEmp ;
+            flag[pos] = 1 ;
+            cout << "Employee inserted at position" << pos << endl ;
+            return ;
+        }
+    }
+    cout << "Hash Table is full" << endl;
+}
+
+void HashTable::deleteID() {
+    int id ;
+    cout << "Enter the employee id to be deleted" ;
+    cin >> id ;
+    int pos = hashFunction(id);
+    if(id <= 0) {
+        cout << "Invalid ID" ;
+    }
+    for(int i = 0 ; i < size ; i++) {
+        pos = (pos + i) % size ;
+        if (flag[pos] == 1 && e[pos].getID() == id){
+            e[pos].clear() ;
+            flag[pos] = 0 ;
+            cout << "Employee with ID " << id << " deleted from position: " << pos << endl;
+            return ;
+        }
+    }    
+    cout << "Not Found" ;
+}
+
+void HashTable::displayTable() {
+    cout << "Position\tID\tName\t\tSalary" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << i << "\t\t";
+        if (flag[i] == 1) { 
+            e[i].display(); 
+        } else {
+            cout << "-\t-\t\t-" << endl; 
+        }
+    }
+}
